36e461acee01f74ba07047cd82899bc35d30da91
ArtP10 <10arturojpinto@gmail.com>
2025-12-15 04:40:20 -0400

    Corregido lo de los viajes

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'viajes_ucab_v16',
+  database: 'dce',
   password: '1234',
   port: 5433,
 });

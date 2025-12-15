de0a65f42fb9870ea91196e41019bc3f809c3db0
ArtP10 <10arturojpinto@gmail.com>
2025-12-15 09:17:40 -0400

    Cambio de db

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'viajes_ucab_v3',
+  database: 'dce',
   password: '1234',
-  port: 5432,
+  port: 5433,
 });
 
 module.exports = pool;
